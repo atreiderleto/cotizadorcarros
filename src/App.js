84dcd618c370d7cd67ce729b9a67dcd0bb0aa627
Alberto Diaz <atreiderleto@gmail.com>
2020-10-24 13:25:37 -0500

    ya listo la apriencia comienza la logica de la app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './componets/Header';
+import styled from '@emotion/styled';
+import Formulario from './componets/Formulario';
+
+const Contenedor = styled.div`
+    max-width: 600px;
+    margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+   background-color: #FFF;
+   padding: 3rem;
+`;
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contenedor >
+      <Header
+        titulo= 'Cotizador de Seguro'
+      />
+
+      <ContenedorFormulario>
+
+          <Formulario />
+
+          
+        
+
+      </ContenedorFormulario>
+
+      </Contenedor>
     </div>
   );
 }
